b6bfc8a1acbc0e793e80fdcb7d62e8a07f5279a6
Ehshan <eveerabangsa@gmail.com>
2014-11-24 04:43:22 +0000

    debugging output removed - in modus operandi

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -7,70 +7,54 @@ MSc Computer Science
 public class FractionCalculator {
 
 	private Fraction storedFraction;
+	private Fraction totalFraction;
 	private Fraction reset;
 	private int operator;
 	private boolean exit;
+	private String errorString;
 	private String userCommand [][];
-	String errorString;
+
 	
 	public FractionCalculator(){
 		storedFraction = new Fraction(0,1);
+		totalFraction = new Fraction(0,1);
 		reset = new Fraction(0,1);
 		operator = 0;
 		exit = false;
 		errorString = "";
 		userCommand = new String[][]{
-				{null,"a", "A", "abs"},
-				{null,"n", "N", "neg"},
-				{null,"c",  "C", "clear"},
-				{null,"q", "Q", "quit"}
+				{"a", "A", "abs",},
+				{"n", "N", "neg",},
+				{"c",  "C", "clear",},
+				{"q", "Q", "quit",}
 		};
 	}
 
+	// main method to check input, develop fraction and assigns operators or user commands
 	public Fraction evaluate(Fraction fraction, String inputString){
 
-		//needs development
-
-		Fraction totalFraction = new Fraction(0,1);
-		System.out.println("total before loop "+ totalFraction);
 		String[] inputArray = inputString.split("\\s");
 		for (int i = 0; i < inputArray .length; i++) {
 			if (checkOperator(inputArray[i])){
 				getOperator(inputArray[i]);
-				//if (operator == 0){
-					//totalFraction = reset;
-					//clear(totalFraction);
-					//System.out.println("After clear tf -- "+ totalFraction);
-					//return totalFraction;
-				//}
 			}else if (checkNumber(inputArray[i])){
 				if(operator == 0) {
-					fraction = fractionBuilder(inputArray [i]);
+					totalFraction= fractionBuilder(inputArray [i]);
 				}else{
-					storedFraction = fractionBuilder(inputArray [i]);
-					System.out.println("second assignment: "+ storedFraction);
-					System.out.println("total after second assignment "+ totalFraction);
+					storedFraction= fractionBuilder(inputArray [i]);
+					totalFraction = sum(totalFraction, storedFraction, operator);
 				}
-				System.out.println("total before "+ totalFraction);
-				System.out.println("fraction before "+ fraction);
-				totalFraction = sum(fraction, storedFraction, operator);
-				System.out.println("total after= : "+ totalFraction);
 			}else if (checkCommand(inputArray [i],userCommand )) {
-				System.out.println("command entered");
 				totalFraction = getCommand(inputArray[i], userCommand, totalFraction);
-			}else if(checkExit(inputArray[i])){
-					exit = true;
 			}else{
-				clear(fraction);
+				clear();
 				errorCollect(inputArray[i], errorString);
 			}
-
 		}
 		if(!errorString.isEmpty()){
 			errorMsg(errorString);
 			clearError(errorString);
 		}
-		storedFraction = totalFraction;
 		return totalFraction;
 	}
 
@@ -90,7 +74,8 @@ public class FractionCalculator {
 	public int getOperator(String input) {
 		if (operator !=0) {
 			System.out.println("Error! Too many operators have spoiled the broth");
-			opReset(operator);
+			clear();
+			opReset();
 			return operator;
 		}
 		char inputChar = input.charAt(0);
@@ -155,85 +140,48 @@ public class FractionCalculator {
 			default:
 				sumFraction = fraction1;
 		}
-		opReset(this.operator);
+		opReset();
 		return sumFraction;
 	}
 
 	// checks for user command characters
 	public boolean checkCommand(String input,String command [][]) {
-		if (input.length() == 1) {
+		boolean match = false;
 			for (int i = 0; i < command.length; i++) {
-				System.out.println((command.length));
-				for (int j = 1; j < command[i].length; j++) {
+				for (int j = 0; j < command[i].length; j++) {
 					if (input.equals(command[i][j])) {
-						return true;
+						match = true;
 					}
 				}
 			}
-		}
-		return false;
+		return match;
 	}
 
 	// assigns method bases on user commands
 	public Fraction getCommand(String input,String userCommand [][],Fraction fraction) {
 		Fraction sumFraction = new Fraction(0,1);
-		System.out.println("passed fraction");
-		System.out.println((fraction));
 		for (int i = 0; i < userCommand .length; i++) {
-			/*
-			if (input.equals(userCommand[0])){
-				System.out.println("ABS CAUGHT");
-				System.out.println("FRACTION passed to method: = "+fraction);
-				fraction.absValue();
-				System.out.println("FRACTION after method: = " + fraction);
-				return fraction;
-			}else if (input.equals(userCommand[1])) {
-				System.out.println("NEG CAUGHT");
-				fraction.negate();
-				return fraction;
-			}else if (input.equals(userCommand[2])){
-				System.out.println("CLEAR CAUGHT");
-				fraction = reset;
-				return fraction;
-			}else //if(input.equals(userCommand[3])) {
-			{
-				System.out.println("QUIT CAUGHT");
-				exit = true;
-				break;
-			}
-
-			*/
-			System.out.println((userCommand[i]));
-			for (int j = 1; j < userCommand[i].length; j++) {
-				System.out.println((userCommand[i][j]));
+			for (int j = 0; j < userCommand[i].length; j++) {
 				if (input.equals(userCommand[0][j])) {
-					System.out.println("ABS CAUGHT");
-					System.out.println("FRACTION passed to method: = "+fraction);
 					sumFraction = fraction.absValue();
-					System.out.println("FRACTION after method: = " + fraction);
 					return sumFraction;
 				}else if (input.equals(userCommand[1][j])){
-					System.out.println("NEG CAUGHT");
 					sumFraction = fraction.negate();
-					System.out.println("FRACTION after neg: = " + fraction);
 					return sumFraction ;
 				}else if (input.equals(userCommand[2][j])){
-					System.out.println("CLEAR CAUGHT");
 					sumFraction = reset;
 					return sumFraction;
-				}else{
-					System.out.println("QUIT CAUGHT");
+				}else if (input.equals(userCommand[3][j])){
 					exit = true;
 					break;
 				}
 			}
 		}
-		System.out.println("fraction below");
-		System.out.println((fraction));
 		return sumFraction;
 	}
 
-	// checks for exit characters
+	// checks for exit characters(no longer needed)
+	/*
 	public boolean checkExit(String input) {
 		if (input.length() == 1) {
 			if (input.equals("q") || input.equals("Q") || input.equals("quit")) {
@@ -242,6 +190,7 @@ public class FractionCalculator {
 		}
 		return false;
 	}
+	*/
 
 	//checks for EOL characters // to be developed
 	public boolean checkError(String input) {
@@ -249,14 +198,13 @@ public class FractionCalculator {
 	}
 
 	// resets fraction values
-	public Fraction clear(Fraction fraction) {
-		fraction = this.reset;
-		return fraction;
+	public void clear() {
+		totalFraction = reset;
 	}
 
 	// re-initialises operator
-	public void opReset(int operator){
-		this.operator = 0;
+	public void opReset(){
+		operator = 0;
 	}
 
 	// builds an error message
@@ -278,7 +226,6 @@ public class FractionCalculator {
 			fraction = evaluate(fraction, userInput);
 			System.out.println("Sum = "+fraction);
 			System.out.println("");
-			System.out.println("operator: "+operator);
 		}
 		System.out.println("Thank you for using my Fraction Calculator");
 	}
@@ -303,7 +250,7 @@ public class FractionCalculator {
 
 	public void errorMsg (String errorString){
 		System.out.println("");
-		System.out.println("Error!"+errorString+" is not a valid input");
+		System.out.println("Error! '"+errorString+"' is not a valid input");
 		System.out.println("");
 	}
 
--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -4,25 +4,46 @@ CW2, Programming in Java;
 MSc Computer Science
  */
 
-//import java.util.Scanner;
 public class FractionCalculatorTest {
-	//private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		//FractionCalculator firstCalculator = new FractionCalculator();
-		//firstCalculator.calculate();
-
 
 		//test with set input
 		FractionCalculator testCalculator = new FractionCalculator();
 
+		//will test one after other, fraction value and operator will be passed to next test
+
 		test(testCalculator, "3/4 + 1/-3 * 7 / 5", new Fraction(0, 1), 0);
+		// calculator should reset
+		test(testCalculator, "what the fudge!", new Fraction(0, 1), 0);
+		// calculate sum
+		test(testCalculator, "1/2 / 3/4 - 8/16", new Fraction(0, 1), 3);
+		// negate sum
+		test(testCalculator, "neg", new Fraction(0, 1), 0);
+		//  reset
+		test(testCalculator, "C", new Fraction(0, 1), 2);
+		// calculate sum to negative fraction
+		test(testCalculator, "1/2 - 3/4", new Fraction(0, 1), 0);
+		// get absolute value
+		test(testCalculator, "a", new Fraction(0, 1), 0);
+		// calculate
+		test(testCalculator, "* 3/5", new Fraction(0, 1), 0);
+		//  reset
+		test(testCalculator, "clear", new Fraction(0, 1), 0);
+		//store fraction
+		test(testCalculator, "3/5", new Fraction(0, 1), 0);
+		//store operator
+		test(testCalculator, "-", new Fraction(0, 1), 0);
+		//result of stored operator and fraction
+		test(testCalculator, "2/5", new Fraction(0, 1), 0);
+
 
 	}
 
-	public static void test(FractionCalculator calculator, String inputString, Fraction storedFraction, int storedOperator) {
+	public static void test(FractionCalculator calculator, String inputString, Fraction fraction, int storedOperator) {
 		System.out.println("Test");
-		System.out.println(calculator.toString());
+		System.out.println(calculator.evaluate(fraction ,inputString).toString());
+		System.out.println("");
 	}
 
 }
